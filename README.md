b71b4fdacba8f6a3c457872ee42eff1c78c83372
YieldRay <24633623+YieldRay@users.noreply.github.com>
2025-01-08 06:29:54 +0000

    auto size

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ TODO:
 
 - [x] mobile view support
 - [ ] support showing sub compat data
-- [ ] auto width/height for svg
+- [x] auto width/height for svg
 - [ ] add a docs site, and can select compat data by ui
 - [ ] support theming (dark mode)
 - [ ] data fetching mode
--- dev/server.tsx
+++ dev/server.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import satori from "satori";
 import { RenderBrowserCompat } from "../src/isomorphic.tsx";
 
+// TODO: use a binary
 async function loadGoogleFont(font: string, text?: string) {
   const url = new URL("https://fonts.googleapis.com/css2");
   url.searchParams.set("family", font);
   if (text) url.searchParams.set("text", text);
-  const css = await (await fetch(url)).text();
-  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/);
+  const css = await fetch(url).then(res => res.text());
+  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/)?.[1];
 
   if (resource) {
-    const response = await fetch(resource[1]);
-    if (response.status == 200) {
+    const response = await fetch(resource);
+    if (response.ok) {
       return await response.arrayBuffer();
     }
   }
