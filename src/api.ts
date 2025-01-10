712837e8873358eab6f33a1e9b1a5c7e4526a4be
YieldRay <yieldray@outlook.com>
2025-01-10 18:53:23 +0800

    fix

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,12 @@
 import type { CompatData, Identifier } from "@mdn/browser-compat-data";
 
-export type Paths = readonly [keyof Omit<CompatData, "__meta" | "browsers">, ...identifiers: Array<keyof Identifier>]
+export type Paths = readonly [keyof Omit<CompatData, "__meta" | "browsers">, ...identifiers: Array<keyof Identifier>];
 
 export async function fetchBcdApi(paths: Paths) {
-    const resp: {
-        borwsers: unknown
-        query: string
-        data: Identifier
-    } = await fetch(`https://bcd.developer.mozilla.org/bcd/api/v0/current/${paths.join('.')}.json`).then(res => res.json())
-    return resp
+  const resp: {
+    browsers: unknown;
+    query: string;
+    data: Identifier;
+  } = await fetch(`https://bcd.developer.mozilla.org/bcd/api/v0/current/${paths.join(".")}.json`).then((res) => res.json());
+  return resp;
 }
-
--- src/react-server.tsx
+++ src/react-server.tsx
@@ -1,12 +1,11 @@
-'use server'
+"use server";
 import React from "react";
-import { fetchBcdApi, type Paths } from "./api.ts"
+import { fetchBcdApi, type Paths } from "./api.ts";
 import { RenderBrowserCompatData } from "./isomorphic.tsx";
 
-export async function RenderBrowserCompat({ paths, compact }: { paths: Paths, compact?: boolean }) {
-    const compat = (await fetchBcdApi(paths)).data.__compat!
-    const name = String(paths[paths.length - 1]);
-    const { support, status, tags } = compat;
-    return <RenderBrowserCompatData {...{ name, support, tags, status, compact }} />
-
+export async function RenderBrowserCompat({ paths, compact }: { paths: Paths; compact?: boolean }) {
+  const compat = (await fetchBcdApi(paths)).data.__compat!;
+  const name = String(paths[paths.length - 1]);
+  const { support, status, tags } = compat;
+  return <RenderBrowserCompatData {...{ name, support, tags, status, compact }} />;
 }
