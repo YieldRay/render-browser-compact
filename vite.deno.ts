b0bcf0ebf71d034427bfb9e5fcbe715f336cd5df
YieldRay <yieldray@outlook.com>
2025-04-16 23:00:37 +0800

    update deno build

--- vite.deno.ts
+++ vite.deno.ts
@@ -2,18 +2,16 @@ import { resolve, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const external = ["@mdn/browser-compat-data", "satori"];
+
 // https://cn.vite.dev/guide/build#library-mode
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
   build: {
     lib: {
       entry: {
@@ -24,7 +22,20 @@ export default defineConfig({
     rollupOptions: {
       output: {
         inlineDynamicImports: true,
+        paths: external.reduce(
+          (paths, pkg) => {
+            paths[pkg] = `https://esm.sh/${pkg}`;
+            return paths;
+          },
+          {} as Record<string, string>,
+        ),
       },
+      external,
     },
+    target: "esnext",
+    minify: "esbuild",
+  },
+  esbuild: {
+    drop: ["console", "debugger"],
   },
 });
